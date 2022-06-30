b4bebf35b94810085af4ed1a55dcfae285d3234e
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-06-30 14:29:46 -0400

    separated category into own component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import CategoryItem from './components/category-item/category.component';
+
 import './categories.styles.scss';
 
 function App() {
@@ -36,17 +38,8 @@ function App() {
 
   return (
     <div className='categories-container'>
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className='category-container'>
-          <div
-            className='background-image'
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
