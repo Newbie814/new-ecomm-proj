6fd8c0b067cd69fa8fb6909d880e3885e63f8135
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-07-05 22:05:04 -0400

    set up env variables for firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+const api = process.env.REACT_APP_FIREBASE_API_KEY;
+
 const SignIn = () => {
   return <div>SignIn</div>;
 };
 
 export default SignIn;
+console.log(api);
+// console.log(process.env.REACT_APP_FIREBASE_API_KEY);
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'ecomm-ws.firebaseapp.com',
+  projectId: 'ecomm-ws',
+  storageBucket: 'ecomm-ws.appspot.com',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
