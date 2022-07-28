e1241db556ea1abb6c862b3ef289a8ab40a41d68
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-07-28 11:16:16 -0400

    finished basic logic of adding to and updating cart.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,8 @@
-import Button from '../../components/button/button.component';
+import { useContext } from 'react';
 
+import { CartContext } from '../../contexts/cart.context';
+
+import Button from '../../components/button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
@@ -7,10 +10,12 @@ import './cart-dropdown.styles.scss';
 import React from 'react';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        {[].map((item) => (
+        {cartItems.map((item) => (
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,6 +1,20 @@
 import { createContext, useState } from 'react';
 
-const addCartItem = (cartItems, productToAdd) => {};
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
@@ -13,9 +27,11 @@ export const CartContextProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
 
-  const addItemToCart = (productToAdd) => {};
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
 
-  const value = { isCartOpen, setIsCartOpen };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
