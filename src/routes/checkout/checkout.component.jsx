2b77266661e35837e936dfdef9ca55a6cd4ebf4c
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-07-30 23:02:13 -0400

    checkout page file structure

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+import './checkout.styles.scss';
+
+const CheckoutComponent = () => {
+  return (
+    <div>
+      <h1>Checkout Page</h1>
+    </div>
+  );
+};
+
+export default CheckoutComponent;
