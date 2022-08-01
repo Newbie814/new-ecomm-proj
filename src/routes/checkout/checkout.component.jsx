502d8a4094ed3abe0bb791229f22d3152c6f7e3e
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-08-01 13:53:43 -0400

    styling for checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -9,21 +9,37 @@ const CheckoutPageComponent = () => {
     useContext(CartContext);
 
   return (
-    <div>
-      <h1>Checkout Page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, price, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
-              <span>{quantity}</span>
-              <span onClick={() => addItemToCart(cartItem)}>increment</span>
-            </div>
-          );
-        })}
+    <div className='checkout-container'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
       </div>
+
+      {cartItems.map((cartItem) => {
+        const { id, name, price, quantity } = cartItem;
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
+            <span>{quantity}</span>
+            <span onClick={() => addItemToCart(cartItem)}>increment</span>
+          </div>
+        );
+      })}
+      <span className='Total'>Total: 0</span>
     </div>
   );
 };
