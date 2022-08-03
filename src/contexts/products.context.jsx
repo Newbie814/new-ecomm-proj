b2f86aec68cba721556db3dfcfe3354660043f24
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-08-02 20:35:54 -0400

    deleted code that was used to perform initial write of products to db

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -2,18 +2,12 @@ import { createContext, useState, useEffect } from 'react';
 
 import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
 
-import SHOP_DATA from '../shop-data.js';
-
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsContextProvider = ({ children }) => {
   const [products, setproducts] = useState([]);
-
-  useEffect(() => {
-    addCollectionAndDocuments('categories', SHOP_DATA);
-  }, []);
 
   const value = { products };
 
