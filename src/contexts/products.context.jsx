1b49e8f97fd573add92a04b7fdbe744955869752
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-08-02 21:01:25 -0400

    set up utility functions to map product db from firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,21 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsContextProvider = ({ children }) => {
-  const [products, setproducts] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
 
   const value = { products };
 
