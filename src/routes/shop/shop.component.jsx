6c46cb7ed93d92687aadd8f710e9a37ad01fcd9b
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-08-06 18:29:34 -0400

    updated shop page to only show preview of each category

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+// import React from 'react';
+
+// import { useContext } from 'react';
+
+// import { CategoriesContext } from '../../contexts/categories.context';
+// import CategoryPreview from '../../components/category-preview/category-preview.component';
+
+// import './shop.styles.scss';
+
+// const Shop = () => {
+//   const { categoriesMap } = useContext(CategoriesContext);
+
+//   return (
+//     <div className='shop-container'>
+//       {Object.keys(categoriesMap).map((title) => {
+//         const products = categoriesMap[title];
+//         return (
+//           <CategoryPreview key={title} title={title} products={products} />
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default Shop;
 
 import { useContext } from 'react';
 
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
 import { CategoriesContext } from '../../contexts/categories.context';
-import ProductCard from '../../components/product-card/product-card.component';
 
 import './shop.styles.scss';
 
@@ -11,19 +36,12 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        // normally use a blank fragment, but this is the only way I could find to get the key to work
-        <React.Fragment key={title}>
-          <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </React.Fragment>
-      ))}
-    </>
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
+      })}
+    </div>
   );
 };
 
