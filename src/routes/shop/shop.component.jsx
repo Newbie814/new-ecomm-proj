65fc626cccaba96dd8dc749370a662ffd89c2254
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-08-06 23:46:46 -0400

    set routing for navigating shop categories. styling broken. Will fix tomorrow

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -26,6 +26,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
@@ -33,6 +34,7 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
     </Routes>
   );
 };
